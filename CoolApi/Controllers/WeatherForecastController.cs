c3892a63a341c0cc0f744176afa1e4d96bd4573d
Michal Nabielec <michal.nabi@gmail.com>
2019-10-28 11:50:33 +0100

    add book model

--- CoolApi/Controllers/WeatherForecastController.cs
+++ CoolApi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace CoolApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Col", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
---
+++ CoolApi/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoolApi.Model
+{
+    public class Book
+    {
+        [Display(Name = "ISBN")]
+        public string Id { get; set; }
+        public string Name { get; set; }
+       public DateTime ReleaseDateTime { get; set; }
+
+
+    }
+}
