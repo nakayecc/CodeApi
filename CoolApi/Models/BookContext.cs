fe543bac0e0e5970c24d0ed04fb455e5ce10ead9
Michal Nabielec <michal.nabi@gmail.com>
2019-10-28 11:58:53 +0100

    add book context

---
+++ CoolApi/Models/BookContext.cs
@@ -0,0 +1,14 @@
+﻿using CoolApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoolApi.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<DbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
